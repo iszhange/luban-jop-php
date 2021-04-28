cbe451108a196e7502addf7582c5056235d818e1
Ken.Zhang <better.man@aliyun.com>
2021-04-28 14:58:54 +0800

    feat(API): 新增
    1、查询推广订单及佣金信息

---
+++ tests/JdUnionOpenOrderRowQueryRequestTest.php
@@ -0,0 +1,32 @@
+﻿<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use LuBan\Jop\Client;
+use LuBan\Jop\Requests\JdUnionOpenOrderRowQueryRequest;
+
+final class JdUnionOpenOrderRowQueryRequestTest extends TestCase
+{
+
+    public function testRequest()
+    {
+        list($appkey, $secret) = include __DIR__ . '/configs.php';
+        $startAt = time() - 3600 * 30;
+
+        $c = new Client();
+        $c->appKey = $appkey;
+        $c->appSecret = $secret;
+        $req = new JdUnionOpenOrderRowQueryRequest();
+        $req->setPageIndex(1);
+        $req->setPageSize(20);
+        $req->setType(1);
+        $req->setStartTime(date('Y-m-d H:i:00', $startAt));
+        $req->setEndTime(date('Y-m-d H:i:00', $startAt + 3500));
+        $result = $c->execute($req);
+        var_dump($result);
+        $this->assertArrayHasKey('jd_union_open_order_row_query_responce', $result);
+
+    }
+
+}
